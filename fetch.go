563a8ae8ff2341d0eaca7d54fda0b240f1dd2d19
George Hotz <geohot@gmail.com>
2016-12-29 14:21:22 -0500

    ok fine i'll get with the go program tabs it is

--- fetch.go
+++ fetch.go
@@ -1,144 +1,161 @@
 package main
 
 import (
-  "bytes"
-  "crypto/md5"
-  "encoding/hex"
-  "fmt"
-  "image"
-  "image/jpeg"
-  "image/png"
-  "io"
-  "log"
-  "os"
-  "golang.org/x/net/html"
-  "math/rand"
-  "io/ioutil"
-  "net/http"
-  "net/url"
-  "strings"
-  "github.com/disintegration/imaging"
-  "time"
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"github.com/disintegration/imaging"
+	"golang.org/x/net/html"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 )
 
 const apiKey string = "6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-"
 
-func fetchImg(ck string) (image.Image) {
-  // fetch the image
-  u, err := url.Parse("http://google.com/recaptcha/api2/payload")
-  if err != nil { log.Fatal(err) }
-  q := u.Query()
-  q.Set("c", ck)
-  q.Set("k", apiKey)
-  u.RawQuery = q.Encode()
+func fetchImg(ck string) image.Image {
+	// fetch the image
+	u, err := url.Parse("http://google.com/recaptcha/api2/payload")
+	if err != nil {
+		log.Fatal(err)
+	}
+	q := u.Query()
+	q.Set("c", ck)
+	q.Set("k", apiKey)
+	u.RawQuery = q.Encode()
 
-  // do fetch
-  imgresponse, err := http.Get(u.String())
-  if err != nil { log.Fatal(err) }
+	// do fetch
+	imgresponse, err := http.Get(u.String())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  img, err := jpeg.Decode(imgresponse.Body)
-  if err != nil { log.Fatal(err) }
+	img, err := jpeg.Decode(imgresponse.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  return img
+	return img
 }
 
 func getChallengeKey() (string, string, image.Image) {
-  // build the request
-  u, err := url.Parse("http://google.com/recaptcha/api/fallback")
-  if err != nil { log.Fatal(err) }
-  q := u.Query()
-  q.Set("k", apiKey)
-  u.RawQuery = q.Encode()
-  //fmt.Println(u)
+	// build the request
+	u, err := url.Parse("http://google.com/recaptcha/api/fallback")
+	if err != nil {
+		log.Fatal(err)
+	}
+	q := u.Query()
+	q.Set("k", apiKey)
+	u.RawQuery = q.Encode()
+	//fmt.Println(u)
 
-  // fetch the webpage
-  response, err := http.Get(u.String())
-  if err != nil { log.Fatal(err) }
-  defer response.Body.Close()
+	// fetch the webpage
+	response, err := http.Get(u.String())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
 
-  // print it
-  bodyBytes, _ := ioutil.ReadAll(response.Body)
+	// print it
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
 
-  z := html.NewTokenizer(ioutil.NopCloser(bytes.NewBuffer(bodyBytes)))
-  tmparr := []string{}
-  ck := ""
-  for {
-    tt := z.Next()
-    switch tt {
-      case html.ErrorToken:
-        return ck, tmparr[3], fetchImg(ck)
-      case html.StartTagToken, html.SelfClosingTagToken:
-        tn, attr := z.TagName()
-        if string(tn) == "img" && attr {
-          for {
-            k, v, attr := z.TagAttr()
-            if string(k) == "src" {
-              //fmt.Println(string(v))
-              u, err := url.Parse(string(v))
-              if err != nil { log.Fatal(err) }
-              q := u.Query()
-              //fmt.Println(q)
-              if q["k"][0] != apiKey { log.Fatal("apiKey doesn't match") }
-              ck = q["c"][0]
-            }
-            if !attr { break }
-          }
-        }
-      case html.TextToken:
-        //fmt.Println(z.Token())
-        tmparr = append(tmparr, z.Token().String())
-    }
-  }
+	z := html.NewTokenizer(ioutil.NopCloser(bytes.NewBuffer(bodyBytes)))
+	tmparr := []string{}
+	ck := ""
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			return ck, tmparr[3], fetchImg(ck)
+		case html.StartTagToken, html.SelfClosingTagToken:
+			tn, attr := z.TagName()
+			if string(tn) == "img" && attr {
+				for {
+					k, v, attr := z.TagAttr()
+					if string(k) == "src" {
+						//fmt.Println(string(v))
+						u, err := url.Parse(string(v))
+						if err != nil {
+							log.Fatal(err)
+						}
+						q := u.Query()
+						//fmt.Println(q)
+						if q["k"][0] != apiKey {
+							log.Fatal("apiKey doesn't match")
+						}
+						ck = q["c"][0]
+					}
+					if !attr {
+						break
+					}
+				}
+			}
+		case html.TextToken:
+			//fmt.Println(z.Token())
+			tmparr = append(tmparr, z.Token().String())
+		}
+	}
 }
 
 func downloader() {
-  bigcnt := 0
-  for {
-    // parse it
-    ck, typ, img := getChallengeKey()
+	bigcnt := 0
+	for {
+		// parse it
+		ck, typ, img := getChallengeKey()
 
-    h := md5.New()
-    io.WriteString(h, ck)
-    hh := hex.EncodeToString(h.Sum(nil))
-    typ = strings.Replace(typ, " ", "_", -1)
-    //fmt.Println(ck, typ, img.Bounds())
-    fmt.Println(bigcnt, hh, typ, img.Bounds())
+		h := md5.New()
+		io.WriteString(h, ck)
+		hh := hex.EncodeToString(h.Sum(nil))
+		typ = strings.Replace(typ, " ", "_", -1)
+		//fmt.Println(ck, typ, img.Bounds())
+		fmt.Println(bigcnt, hh, typ, img.Bounds())
 
-    if img.Bounds() != image.Rect(0,0,300,300) {
-      log.Fatal("IMAGE IS THE WRONG SIZE")
-    }
+		if img.Bounds() != image.Rect(0, 0, 300, 300) {
+			log.Fatal("IMAGE IS THE WRONG SIZE")
+		}
 
-    // write it
-    os.MkdirAll("imgs/"+typ, 0755)
+		// write it
+		os.MkdirAll("imgs/"+typ, 0755)
 
-    cnt := 0
-    for h := 0; h < 300; h += 100 {
-      for w := 0; w < 300; w += 100 {
-        lilimg := imaging.Crop(img, image.Rect(w,h,w+100,h+100))
+		cnt := 0
+		for h := 0; h < 300; h += 100 {
+			for w := 0; w < 300; w += 100 {
+				lilimg := imaging.Crop(img, image.Rect(w, h, w+100, h+100))
 
-        fn := fmt.Sprintf("imgs/%s/%s_%d.png", typ, hh, cnt)
-        f, err := os.OpenFile(fn, os.O_CREATE | os.O_WRONLY, 0644)
-        if err != nil { log.Fatal(err) }
-        png.Encode(f, lilimg)
-        f.Close()
+				fn := fmt.Sprintf("imgs/%s/%s_%d.png", typ, hh, cnt)
+				f, err := os.OpenFile(fn, os.O_CREATE|os.O_WRONLY, 0644)
+				if err != nil {
+					log.Fatal(err)
+				}
+				png.Encode(f, lilimg)
+				f.Close()
 
-        cnt += 1
-      }
-    }
-    bigcnt += 1
-    time.Sleep(time.Duration(rand.Intn(2000))*time.Millisecond)
-  }
+				cnt += 1
+			}
+		}
+		bigcnt += 1
+		time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
+	}
 }
 
 func main() {
-  fmt.Println("my first golang program")
+	fmt.Println("my first golang program")
 
-  /*for i := 0; i < 8; i += 1 {
-    go downloader()
-  }*/
-  downloader()
+	/*for i := 0; i < 8; i += 1 {
+	  go downloader()
+	}*/
+	downloader()
 
-  // move on
-  fmt.Println("still alive!")
+	// move on
+	fmt.Println("still alive!")
 }
-
--- loader.go
+++ loader.go
@@ -11,142 +11,152 @@ TODO: don't be cheater and use python only golang pull request accepted
 */
 
 import (
-  "path/filepath"
-  "image"
-  "image/png"
-  "math/rand"
-  "strings"
-  _"fmt"
-  "os"
-  "log"
-  _"github.com/disintegration/imaging"
-  "github.com/disintegration/gift"
-  "github.com/NOX73/go-neural"
-  "github.com/NOX73/go-neural/learn"
+	"fmt"
+	"github.com/disintegration/gift"
+	_ "github.com/disintegration/imaging"
+	"image"
+	"image/png"
+	"log"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strings"
+	//"github.com/NOX73/go-neural"
+	//"github.com/NOX73/go-neural/learn"
+	//"github.com/sajari/random-forest/RF"
 )
 
 func randomArray(n int) []float32 {
-  ret := make([]float32, n)
-  for i := 0; i < n; i++ {
-    ret[i] = (rand.Float32()-0.5)*5
-  }
-  return ret
+	ret := make([]float32, n)
+	for i := 0; i < n; i++ {
+		ret[i] = (rand.Float32() - 0.5) * 5
+	}
+	return ret
 }
 
 func main() {
-  log.Print("use log so we don't have to put an underscore before the import")
+	log.Print("use log so we don't have to put an underscore before the import")
 
-  type Example struct {
-    features []float64
-    yes bool
-  }
+	type Example struct {
+		features []float32
+		category string
+	}
 
-  paths := make(chan string)
-  processed := make(chan Example)
+	paths := make(chan string)
+	processed := make(chan Example)
 
-  // the Seed for the network is 7
-  rand.Seed(7)
+	// the Seed for the network is 7
+	rand.Seed(7)
 
-  g := gift.New(
-    // edge detector
-    gift.Convolution(
-      []float32{
-        -1, -1, -1,
-        -1,  8, -1,
-        -1, -1, -1,
-      },
-      false, false, false, 0.0),
-    // is this max pool?
-    gift.Maximum(2, true),
-    gift.Resize(50, 0, gift.LinearResampling),
+	g := gift.New(
+		// edge detector
+		gift.Convolution(
+			[]float32{
+				-1, -1, -1,
+				-1, 8, -1,
+				-1, -1, -1,
+			},
+			false, false, false, 0.0),
+		// is this max pool?
+		gift.Maximum(2, true),
+		gift.Resize(50, 0, gift.LinearResampling),
 
-    // random 5x5 conv, hmm but like the color channels bro this is a shit neural network
-    gift.Convolution(
-      randomArray(25),
-      false, false, false, 0.0),
-    // is this max pool?
-    gift.Maximum(2, true),
-    gift.Resize(25, 0, gift.LinearResampling),
+		// random 5x5 conv, hmm but like the color channels bro this is a shit neural network
+		gift.Convolution(
+			randomArray(25),
+			false, false, false, 0.0),
+		// is this max pool?
+		gift.Maximum(2, true),
+		gift.Resize(25, 0, gift.LinearResampling),
 
-    // random 3x3 conv, hmm but like the color channels bro this is a shit neural network
-    gift.Convolution(
-      randomArray(9),
-      false, false, false, 0.0),
-    // is this max pool?
-    gift.Maximum(2, true),
-    gift.Resize(10, 0, gift.LinearResampling),
+		// random 3x3 conv, hmm but like the color channels bro this is a shit neural network
+		gift.Convolution(
+			randomArray(9),
+			false, false, false, 0.0),
+		// is this max pool?
+		gift.Maximum(2, true),
+		gift.Resize(10, 0, gift.LinearResampling),
 
-    // 300 features one for each spartan RIP
-  )
+		// 300 features one for each spartan RIP
+	)
 
-  n := neural.NewNetwork(300, []int{100,20,1})
-  n.RandomizeSynapses()
+	//n := neural.NewNetwork(300, []int{100,20,1})
+	//n.RandomizeSynapses()
 
-  // forest builder
-  go func() {
-    // is this a proper design pattern?
-    // probs not it's awkward ROS node shit
-    for {
-      sample := <-processed
-      //fmt.Println(sample)
+	// forest builder
+	go func() {
+		// is this a proper design pattern?
+		// probs not it's awkward ROS node shit
+		for {
+			sample := <-processed
+			fmt.Println(sample)
 
-      // ugh no inline if?
-      prob := []float64{0.01}
-      if sample.yes {
-        prob = []float64{0.4}
-      }
+			// ugh no inline if?
+			/*prob := []float64{0.01}
+			  if sample.yes {
+			    prob = []float64{0.4}
+			  }
 
-      learn.Learn(n, sample.features, prob, 0.05)
+			  learn.Learn(n, sample.features, prob, 0.05)
 
-      println(learn.Evaluation(n, sample.features, prob))
-    }
-  }()
+			  println(prob[0], learn.Evaluation(n, sample.features, prob))*/
+		}
+	}()
 
-  // image loader and network runner 
-  go func() {
-    for {
-      path := <-paths
+	// image loader and network runner
+	go func() {
+		for {
+			path := <-paths
 
-      // load the image, this is 5 lines
-      // i hate all this error handling does go have exceptions?
-      f, err := os.Open(path)
-      if err != nil { log.Fatal(err) }
-      img, err := png.Decode(f)
-      if err != nil { log.Fatal(err) }
-      f.Close()
+			// load the image, this is 5 lines
+			// i hate all this error handling does go have exceptions?
+			f, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err, path)
+			}
+			img, err := png.Decode(f)
+			if err != nil {
+				log.Fatal(err, path)
+			}
+			f.Close()
 
 			dst := image.NewRGBA(g.Bounds(img.Bounds()))
-      g.Draw(dst, img)
+			g.Draw(dst, img)
 
-      // extract features
-      // i can write much better than this wow shit
-      ret := make([]float64, 300)
-      cnt := 0
-      for i:=0;i<400;i++ {
-        if i%4 == 3 { continue }
-        ret[cnt] = float64(dst.Pix[i]) / 256.0
-        cnt += 1
-      }
+			// extract features
+			// i can write much better than this wow shit
+			ret := make([]float32, 300)
+			cnt := 0
+			for i := 0; i < 400; i++ {
+				if i%4 == 3 {
+					continue
+				}
+				ret[cnt] = float32(dst.Pix[i]) / 256.0
+				cnt += 1
+			}
 
-      //fmt.Println(path)
+			processed <- Example{features: ret, category: strings.Split(path, "/")[1]}
 
-      processed <- Example{features: ret, yes: strings.Contains(path, "street_signs")}
-
-      //imaging.Save(dst, "dst.png")
-      //println(dst)
-    }
-  }()
-
-	justone := true
-  filepath.Walk("imgs/", func(path string, finfo os.FileInfo, err error) error {
-    if finfo.IsDir() { return nil }
-		if justone {
-			paths <- path
-			justone = true
+			//imaging.Save(dst, "dst.png")
+			//println(dst)
 		}
-    return nil
-  });
+	}()
 
+	files := []string{}
 
+	filepath.Walk("imgs/", func(path string, finfo os.FileInfo, err error) error {
+		if finfo.IsDir() {
+			return nil
+		}
+		//paths <- path
+		files = append(files, path)
+		return nil
+	})
+	fmt.Println("files list built")
+	println(len(files))
+
+	/*perm := rand.Perm(len(files))
+	  for _, v := range perm {
+	    paths <- files[v]
+	  }*/
 }
-
